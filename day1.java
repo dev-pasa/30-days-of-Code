7ff00405fc47afe610d4918b43044231cc7737a6
Dev Shrestha <dev5@uw.edu>
2018-09-18 11:37:58 -0700

    Update day1.java

--- day1.java
+++ day1.java
@@ -3,21 +3,36 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
-public class Solution {
-	public static void main(String[] args) {
-        // Create a Scanner object to read input from stdin.
-		Scanner scan = new Scanner(System.in); 
-		
-		// Read a full line of input from stdin and save it to our variable, inputString.
-		String inputString = scan.nextLine(); 
 
-		// Close the scanner object, because we've finished reading 
-        // all of the input from stdin needed for this challenge.
-		scan.close(); 
-      
-		// Print a string literal saying "Hello, World." to stdout.
-		System.out.println("Hello, World.");
-      
-	    // TODO: Write a line of code here that prints the contents of inputString to stdout.
-	}
-}
+public class Solution {
+	
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+        /* Declare second integer, double, and String variables. */
+
+        int i2;
+        double d2;
+        String s2;
+                   
+
+        /* Read and save an integer, double, and String to your variables.*/
+        
+        i2 = scan.nextInt();
+        d2 = scan.nextDouble();
+        scan.nextLine();
+        s2 = scan.nextLine();
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(i + i2);
+        /* Print the sum of the double variables on a new line. */
+		System.out.println(d + d2);
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+            System.out.println(s.concat(s2));
+            
+        scan.close();
