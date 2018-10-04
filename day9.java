085c01edf5bbcbd810f3dccf094bbf5cc6d1bdee
Dev Shrestha <dev5@uw.edu>
2018-10-04 09:24:05 -0700

    Update day9.java

--- day9.java
+++ day9.java
@@ -4,23 +4,24 @@ import java.security.*;
 import java.text.*;
 import java.util.*;
 import java.util.concurrent.*;
-import java.util.regex.*;
+import java.util.Scanner;
 
 public class Solution {
     // Complete the factorial function below.
     public static int factorial(int n) {
+        
         if (n <= 0){
-            return 0;
+            return 1;
         } else{
-            return n + factorial(n - 1);
+            return factorial(n - 1)*n;
         }
     }
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
         int n = scanner.nextInt();
-        int result = factorial(n);
-        System.out.print(result);
-               scanner.close();
+        scanner.close();
+           System.out.print(factorial(n));
+               
     }
 }
