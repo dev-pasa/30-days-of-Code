c6ac2973216cca6490b04de451aa81f94b40f22f
Dev Shrestha <dev5@uw.edu>
2018-10-04 10:30:56 -0700

    Create day10.java

---
+++ day10.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        
+        int sum = 0, count = 0;
+        while(n > 0){
+        if (n %2 == 1){
+                sum++;}
+        if ( sum > count){
+            count = sum;
+        } else{
+            sum = 0;
+        }
+        n = n/2;
+        }
+        System.out.println(count);
+        scanner.close();
+    }
+    }
+
